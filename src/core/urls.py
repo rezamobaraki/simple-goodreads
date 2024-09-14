4a07e4db3fcfa8b21ddcc57b54464f3414097f0b
Reza Mobaraki <rezam578@gmail.com>
2024-09-15 02:44:26 +0330

    Fix(): debug toolbar urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,3 +9,12 @@ urlpatterns = [
     path("api/v1/", include("routers.urls")),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    debug_toolbar_urls = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+
+    urlpatterns += debug_toolbar_urls
