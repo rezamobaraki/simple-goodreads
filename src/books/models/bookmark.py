5f0a2c04d664a50bec0e96196fb31f5d5c71fdd8
Reza Mobaraki <rezam578@gmail.com>
2024-09-15 17:44:47 +0330

    chore(books): add Bookmark model

---
+++ src/books/models/bookmark.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from commons.models import BaseModel
+
+
+class Bookmark(BaseModel):
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name='bookmarks')
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='bookmarks')
+
+    class Meta:
+        unique_together = ('book', 'user')
+
+    def __str__(self):
+        return f"{self.user.username}'s bookmark of {self.book.title}"
---
+++ src/books/models/review.py
@@ -0,0 +1,17 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from commons.models import BaseModel
+
+
+class Review(BaseModel):
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('book', 'user')
+
+    def __str__(self):
+        return f"{self.user.email}'s review of {self.book.title}"
