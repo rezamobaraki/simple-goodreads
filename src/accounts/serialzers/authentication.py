921bd1e46729c81bc785533dbfe367e259472553
Reza Mobaraki <rezam578@gmail.com>
2024-09-15 16:43:56 +0330

    chore(auth): add validator to email and password field

--- src/accounts/serialzers/authentication.py
+++ src/accounts/serialzers/authentication.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from django.core.validators import EmailValidator
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
@@ -9,18 +10,20 @@ User = get_user_model()
 
 
 class AuthenticationRequestSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(write_only=True, required=True, validators=[EmailValidator])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    access_token = serializers.CharField(read_only=True)
+    refresh_token = serializers.CharField(read_only=True)
 
-    def validate_password(self, value):
-        validate_password(value)
-        return value
-
-    def create(self, validated_data):
-        user = auth_create_user(email=validated_data["email"], password=validated_data["password"])
+    def validate(self, values):
+        user = auth_create_user(email=values["email"], password=values["password"])
         if not user:
             raise serializers.ValidationError("Invalid email or password")
+        self.validated_data["user"] = user
+        return values
 
+    def create(self, validated_data):
+        user = validated_data["user"]
         refresh_token = TokenObtainPairSerializer.get_token(user)
         access_token = str(refresh_token.access_token)
-        return {'user': user, 'access_token': access_token}
+        return {"access_token": access_token, "refresh_token": str(refresh_token)}
